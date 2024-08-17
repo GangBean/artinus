09a25a73c2980ca7edcce11eb4bc2bbf6d4c1e8f
GangBean <xbox2537@gmail.com>
2024-08-18 08:55:46 +0900

    feat: add description to request and response classes #8

--- src/main/java/com/artinus/subscription/api/request/SubscriptionRequest.java
+++ src/main/java/com/artinus/subscription/api/request/SubscriptionRequest.java
@@ -2,6 +2,7 @@ package com.artinus.subscription.api.request;
 
 import com.artinus.subscription.api.entity.SubscriptionState;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,8 +10,12 @@ import lombok.NoArgsConstructor;
 
 @Builder @AllArgsConstructor
 @Getter @NoArgsConstructor
+@Schema(description = "구독 요청 정보를 담은 클래스")
 public class SubscriptionRequest {
+    @Schema(description = "사용자의 휴대폰 번호", example = "010-1111-1111")
     private String cellPhoneNumber;
+    @Schema(description = "요청 채널 ID", example = "1")
     private Long channelId;
+    @Schema(description = "요청 구독 상태", example = "NORMAL")
     private SubscriptionState subscriptionState;
 }
--- src/main/java/com/artinus/subscription/api/response/HistoryListResponse.java
+++ src/main/java/com/artinus/subscription/api/response/HistoryListResponse.java
@@ -2,12 +2,15 @@ package com.artinus.subscription.api.response;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Builder @Getter
 @EqualsAndHashCode
+@Schema(description = "구독 이력 조회 결과를 담은 클래스")
 public class HistoryListResponse {
+    @Schema(description = "구독 이력 목록", example = "[{...}, {...}]")
     private final List<HistoryResponse> histories;
 }
